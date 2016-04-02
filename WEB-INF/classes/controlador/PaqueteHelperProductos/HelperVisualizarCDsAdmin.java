6f055509ae67142bbcbdf6a5c1c421d5340caa09
Rubén Osorio López <gladislacebrafeliz@gmail.com>
2016-04-02 21:23:52 +0200

    Segunda actualizacion de helpers
    yolé

---
+++ WEB-INF/classes/controlador/PaqueteHelperProductos/HelperVisualizarCDsAdmin.java
@@ -0,0 +1,10 @@
+package controlador.PaqueteHelperProductos;
+
+public class HelperVisualizarCDsAdmin implements controlador.Helper {
+
+    @Override
+    public void ejecutar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
