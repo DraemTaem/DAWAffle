1464d506635ff58c38f9308ac9436a411b1ad30c
Rubén Osorio López <gladislacebrafeliz@gmail.com>
2016-04-02 21:20:39 +0200

    Actualizados paquetesHelperCosas y cosas del modelo
    Muy patch y mucho patch

---
+++ WEB-INF/classes/modelo/pckPedidos/LineaCarrito.java
@@ -0,0 +1,56 @@
+package modelo.pckProductos;
+
+public class LineaCarrito {
+
+    private Producto producto;
+    private int cantidad;
+    private float precioLinea;
+
+    public LineaCarrito(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        actualizarPrecioDeLaLinea();
+    }
+
+    public int actualizar(Tienda tienda) {
+
+        Producto p = tienda.getProductoById(this.producto.getID());
+        if (p == null) {
+            return -1;
+        }
+
+        this.producto = p;
+        
+        if(this.cantidad>this.producto.getStock()){
+            this.cantidad=this.producto.getStock();
+            return -2;
+        }
+        
+        actualizarPrecioDeLaLinea();
+
+        return 0;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+        actualizarPrecioDeLaLinea();
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public final void actualizarPrecioDeLaLinea() {
+        this.precioLinea = this.producto.getPrecio() * this.cantidad;
+    }
+
+    public float getPrecioLinea() {
+        actualizarPrecioDeLaLinea();
+        return precioLinea;
+    }
+
+}
