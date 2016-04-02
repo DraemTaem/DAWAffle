1d97663496ce26b11dd16c58bb318e25f3e4de95
Rubén Osorio López <gladislacebrafeliz@gmail.com>
2016-04-02 21:36:25 +0200

    Revert "Segunda actualizacion de helpers"
    This reverts commit 6f055509ae67142bbcbdf6a5c1c421d5340caa09.

--- WEB-INF/classes/controlador/PaqueteHelperPago/HelperConfirmarPago.java
+++ WEB-INF/classes/controlador/PaqueteHelperPago/HelperConfirmarPago.java
@@ -7,32 +7,25 @@ import modelo.Pedido;
 import modelo.pckUsuarios.Usuario;
 
 public class HelperConfirmarPago implements controlador.Helper {
-
+    
     private HttpSession sesion;
-
+    
     public HelperConfirmarPago(HttpSession sesion) {
         this.sesion = sesion;
     }
-
+    
     @Override
-    public boolean ejecutar() {
-
-        Pedido pedido = (Pedido) sesion.getAttribute("pedido");
-
-        if (pedido.registrarPedido() == false) {
-            return false;
-        } //TODO registrar pedido
+    public void ejecutar() {
         
-        pedido.enviarCorreoUsuario();
-
+        Pedido pedido = (Pedido) sesion.getAttribute("pedido");
+        //Por ahora no hacemos nada con el pedido a la hora de guardarlo.
         sesion.setAttribute("mensaje", "Se envia el correo a: " + pedido.getUsuario().getCorreoElectronico());
 
         //Borramos el pedido de la sesión y reseteamos el carrito al usuario
         sesion.removeAttribute("pedido");
         Usuario usuario = (Usuario) sesion.getAttribute("usuario");
         usuario.setCarrito(new Carrito());
-
-        return true;
+        
     }
-
+    
 }
